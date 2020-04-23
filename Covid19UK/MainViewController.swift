ab296a62501baee66b027173d5242e66bee1624f
Wan-Nuo, Chiu <sunnusbi@gmail.com>
2020-04-23 20:14:47 +0100

    v1

---
+++ Covid19UK/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Covid19UK
+//
+//  Created by ChiuWanNuo on 23/04/2020.
+//  Copyright © 2020 ChiuWanNuo. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
